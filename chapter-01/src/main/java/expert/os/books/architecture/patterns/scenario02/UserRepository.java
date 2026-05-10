b6d49270e7733bfcdf803c9ee2a401c1310d173a
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-10 11:56:44 +0100

    chore: change UserRepository from interface to class and implement findById method for scenario02 module

--- chapter-01/src/main/java/expert/os/books/architecture/patterns/scenario02/UserRepository.java
+++ chapter-01/src/main/java/expert/os/books/architecture/patterns/scenario02/UserRepository.java
@@ -1,6 +1,8 @@
 package expert.os.books.architecture.patterns.scenario02;
 
 // The Abstraction: Supposed to completely hide the storage mechanism
-public interface UserRepository {
-    User findById(String id);
+public class UserRepository {
+    User findById(String id) {
+        return new User();
+    }
 }

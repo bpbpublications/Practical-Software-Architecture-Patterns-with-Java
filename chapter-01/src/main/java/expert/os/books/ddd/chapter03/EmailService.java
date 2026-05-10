09f2723a1bbf26c49bb8af3678d1e97cdf7dfb68
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-10 07:55:58 +0100

    remove package name

--- chapter-01/src/main/java/expert/os/books/ddd/chapter03/EmailService.java
+++
@@ -1,9 +0,0 @@
-package expert.os.books.ddd.chapter03;
-
-public class EmailService {
-
-    public void sendHiringNotification(Player player, Team team) {
-        // Email sending logic here
-        System.out.println("Email sent: Player " + player.getName() + " hired by team " + team.getName());
-    }
-}
--- chapter-01/src/main/java/expert/os/books/ddd/chapter03/player/Position.java
+++
@@ -1,5 +0,0 @@
-package expert.os.books.ddd.chapter03.player;
-
-public enum Position {
-    GOALKEEPER, DEFENDER, MIDFIELDER, FORWARD;
-}

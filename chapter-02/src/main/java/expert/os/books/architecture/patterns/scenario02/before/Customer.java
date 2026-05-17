e55520577603a399acf36061998734fa51eac8e0
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-17 13:13:00 +0100

    chore: add initial classes for scenario02 before Law of Demeter refactor

---
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario02/before/Customer.java
@@ -0,0 +1,7 @@
+package expert.os.books.architecture.patterns.scenario02.before;
+
+public class Customer {
+    public Wallet getWallet() {
+        return null;
+    }
+}
---
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario02/before/PaymentMethod.java
@@ -0,0 +1,4 @@
+package expert.os.books.architecture.patterns.scenario02.before;
+
+public class PaymentMethod {
+}

c209286a70311ee05d1bdea2076602f3c7d0abdb
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-17 09:53:58 +0100

    chore: add Order class with applyDiscount method stub

---
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario01/rich/Order.java
@@ -0,0 +1,7 @@
+package expert.os.books.architecture.patterns.scenario01.rich;
+
+public class Order {
+    public void applyDiscount(Mone discount) {
+        // Apply discount logic here
+    }
+}

c0fc273d51a6297a2a02c6a6191f6ac0e3154e58
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-17 09:41:40 +0100

    chore: add Customer class with loyalty points management

---
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario01/Customer.java
@@ -0,0 +1,8 @@
+package expert.os.books.architecture.patterns.scenario01;
+
+class Customer {
+    private int loyaltyPoints;
+
+    public int getLoyaltyPoints() { return loyaltyPoints; }
+    public void setLoyaltyPoints(int points) { this.loyaltyPoints = points; }
+}

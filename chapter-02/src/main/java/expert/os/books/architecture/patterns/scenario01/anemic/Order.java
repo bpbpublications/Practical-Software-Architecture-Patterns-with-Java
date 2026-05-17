0a865a1645a296fcf86213d503102fbb27e49542
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-17 09:43:24 +0100

    chore: add applyDiscount method to Order class

--- chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario01/anemic/Order.java
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario01/anemic/Order.java
@@ -1,4 +1,7 @@
 package expert.os.books.architecture.patterns.scenario01.anemic;
 
 public class Order {
+    public void applyDiscount(double v) {
+            // Apply discount logic here
+    }
 }

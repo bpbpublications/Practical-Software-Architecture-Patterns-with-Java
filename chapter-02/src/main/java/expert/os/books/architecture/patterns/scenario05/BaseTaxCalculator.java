9b8165baccc12f71f937411a5ba6b96176359522
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-18 18:02:25 +0100

    chore: update BaseTaxCalculator to use BigDecimal for base calculation

--- chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario05/BaseTaxCalculator.java
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario05/BaseTaxCalculator.java
@@ -1,7 +1,9 @@
 package expert.os.books.architecture.patterns.scenario05;
 
+import java.math.BigDecimal;
+
 abstract class BaseTaxCalculator {
-    protected double calculateBase(double amount) {
-        return amount * 0.10;
+    protected BigDecimal calculateBase(BigDecimal amount) {
+        return amount.multiply( BigDecimal.valueOf(0.10));
     }
 }

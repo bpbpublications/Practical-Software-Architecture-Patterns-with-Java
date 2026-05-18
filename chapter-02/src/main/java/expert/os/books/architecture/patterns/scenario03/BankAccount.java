d0e8390a4a53fd638d5d885a8762d655c783f59e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-18 07:35:39 +0100

    chore: make builder() method in BankAccount static for easier access

--- chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario03/BankAccount.java
+++ chapter-02/src/main/java/expert/os/books/architecture/patterns/scenario03/BankAccount.java
@@ -21,7 +21,7 @@ public class BankAccount {
     }
 
     //usage sample BankAccount account = BankAccount.builder().accountNumber("123456789").balance(new BigDecimal("1000.00")).build();
-    BankAccountBuilder builder() {
+    static BankAccountBuilder builder() {
         return new BankAccountBuilder();
     }
 }

46bcd232efd184ed8a8b40f0298c776afd256baa
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-19 17:56:02 +0100

    remove scenario 05

--- chapter-11/src/main/java/expert/os/books/architecture/patterns/scenario05/after/BaseTaxStrategy.java
+++
@@ -1,7 +0,0 @@
-package expert.os.books.architecture.patterns.scenario05.after;
-
-import java.math.BigDecimal;
-
-interface BaseTaxStrategy {
-    BigDecimal calculateBase(BigDecimal amount);
-}
--- chapter-11/src/main/java/expert/os/books/architecture/patterns/scenario05/before/USATaxCalculator.java
+++
@@ -1,9 +0,0 @@
-package expert.os.books.architecture.patterns.scenario05.before;
-
-import java.math.BigDecimal;
-
-class USATaxCalculator extends BaseTaxCalculator {
-    public BigDecimal calculate(BigDecimal amount) {
-        return calculateBase(amount).add(BigDecimal.valueOf(5.00)); // Tightly coupled to parent's implementation
-    }
-}

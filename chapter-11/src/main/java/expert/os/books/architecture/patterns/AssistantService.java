8dcabcdb567f604550519c148c8b5593a1cef9e1
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-19 18:11:02 +0100

    import RegisterAIService and SystemMessage annotations in AssistantService

--- chapter-11/src/main/java/expert/os/books/architecture/patterns/AssistantService.java
+++ chapter-11/src/main/java/expert/os/books/architecture/patterns/AssistantService.java
@@ -1,5 +1,8 @@
 package expert.os.books.architecture.patterns;
 
+import dev.langchain4j.cdi.spi.RegisterAIService;
+import dev.langchain4j.service.SystemMessage;
+
 @RegisterAIService
 public interface AssistantService {
 

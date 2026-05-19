0a7eb13dd53739f37586b1afad668904b5f0888e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-19 18:28:16 +0100

    annotate AssistantService with ApplicationScoped for CDI context

--- chapter-11/src/main/java/expert/os/books/architecture/patterns/AssistantService.java
+++ chapter-11/src/main/java/expert/os/books/architecture/patterns/AssistantService.java
@@ -2,8 +2,10 @@ package expert.os.books.architecture.patterns;
 
 import dev.langchain4j.cdi.spi.RegisterAIService;
 import dev.langchain4j.service.SystemMessage;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @RegisterAIService
+@ApplicationScoped
 public interface AssistantService {
 
     @SystemMessage("You are a helpful assistant.")

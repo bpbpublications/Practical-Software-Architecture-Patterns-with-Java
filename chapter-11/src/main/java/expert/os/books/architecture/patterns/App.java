e45f418d5c8e36350d58d3865b400569282e406f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-05-20 07:28:28 +0100

    activate and deactivate request context in main application entrypoint

--- chapter-11/src/main/java/expert/os/books/architecture/patterns/App.java
+++ chapter-11/src/main/java/expert/os/books/architecture/patterns/App.java
@@ -1,5 +1,6 @@
 package expert.os.books.architecture.patterns;
 
+import jakarta.enterprise.context.control.RequestContextController;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
@@ -7,9 +8,12 @@ public class App {
 
     public static void main(String[] args) {
         try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            RequestContextController requestContextController = container.select(RequestContextController.class).get();
+            requestContextController.activate();
             var service = container.select(AssistantService.class).get();
-            String chat = service.chat("Hello, how are you?");
+            String chat = service.chat("How can I create a data structure to handle with my cards?");
             System.out.println(chat);
+            requestContextController.deactivate();
         }
     }
 }

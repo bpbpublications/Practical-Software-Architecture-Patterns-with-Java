f21f8954d46c12b8db67bb6f7cf798ef8c353f0c
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-07 12:58:23 +0100

    chore: remove structure class from previous chapter"

--- chapter-03/src/main/java/expert/os/books/architecture/patterns/scenario06/EmailReceiptSender.java
+++
@@ -1,9 +0,0 @@
-package expert.os.books.architecture.patterns.scenario06;
-
-import java.math.BigDecimal;
-
-class EmailReceiptSender implements ReceiptSender {
-    public void sendReceipt(BigDecimal amount) {
-        System.out.println("Sending Email Receipt for: " + amount);
-    }
-}

6323c440f1177b565986f87a434c3e83c33eeb05
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-14 17:42:11 +0100

    add jakarta rest application class to enable rest base path

--- chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/adapter/in/ArticleResource.java
+++ chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/adapter/in/ArticleResource.java
@@ -1,11 +1,15 @@
 package expert.os.books.architecture.patterns.hexagon.core.adapter.in;
 
+import expert.os.books.architecture.patterns.hexagon.core.Article;
 import expert.os.books.architecture.patterns.hexagon.core.port.in.PublishArticleUseCase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.logging.Logger;
 
@@ -15,7 +19,9 @@ import java.util.logging.Logger;
  */
 @ApplicationScoped
 @Path("articles")
-class ArticleResource {
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
+public class ArticleResource {
     private static final Logger LOGGER = Logger.getLogger(ArticleResource.class.getName());
 
     private final PublishArticleUseCase publishUseCase;
@@ -25,9 +31,13 @@ class ArticleResource {
         this.publishUseCase = publishUseCase;
     }
 
+    ArticleResource() {
+        this.publishUseCase = null;
+    }
+
     @Path("{pathId}/publish")
     @POST
-    public void handlePostRequest(@PathParam("pathId") String pathId) {
+    public Article handlePostRequest(@PathParam("pathId") String pathId) {
         LOGGER.info("[REST Adapter] Received HTTP POST to /articles/" + pathId + "/publish");
         publishUseCase.publishArticle(pathId);
     }

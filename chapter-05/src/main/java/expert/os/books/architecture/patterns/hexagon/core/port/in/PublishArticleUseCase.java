6518764266edbd2883a0d91caf3ce0f4ebfe038b
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-14 17:42:42 +0100

    return published article from rest post endpoint

--- chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/port/in/PublishArticleUseCase.java
+++ chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/port/in/PublishArticleUseCase.java
@@ -1,10 +1,12 @@
 package expert.os.books.architecture.patterns.hexagon.core.port.in;
 
 
+import expert.os.books.architecture.patterns.hexagon.core.Article;
+
 /**
  * INBOUND PORT (Driving)
  * Defines the Use Case. This is how the outside world commands the application.
  */
 public interface PublishArticleUseCase {
-    void publishArticle(String articleId);
+    Article publishArticle(String articleId);
 }

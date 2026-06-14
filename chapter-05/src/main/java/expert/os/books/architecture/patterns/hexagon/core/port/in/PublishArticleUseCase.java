de3443103a8102f716c4bac48bf0ee1101c3fbb3
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-14 17:11:40 +0100

    add article class with title and publish validation

---
+++ chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/port/in/PublishArticleUseCase.java
@@ -0,0 +1,10 @@
+package expert.os.books.architecture.patterns.hexagon.core.port.in;
+
+
+/**
+ * INBOUND PORT (Driving)
+ * Defines the Use Case. This is how the outside world commands the application.
+ */
+interface PublishArticleUseCase {
+    void publishArticle(String articleId);
+}

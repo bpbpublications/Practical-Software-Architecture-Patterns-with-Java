daca5b4ff27bcf930bd3360f4b0e3a87e4c9e608
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-14 17:42:32 +0100

    invoke publish article use case from hexagonal main

--- chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/adapter/in/HexagonalMain.java
+++ chapter-05/src/main/java/expert/os/books/architecture/patterns/hexagon/core/adapter/in/HexagonalMain.java
@@ -1,5 +1,6 @@
 package expert.os.books.architecture.patterns.hexagon.core.adapter.in;
 
+import expert.os.books.architecture.patterns.hexagon.core.port.in.PublishArticleUseCase;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 
@@ -7,8 +8,8 @@ public class HexagonalMain {
 
     public static void main(String[] args) {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-
-
+        var useCase = container.select(PublishArticleUseCase.class).get();
+        useCase.publishArticle("123");
         }
     }
 }

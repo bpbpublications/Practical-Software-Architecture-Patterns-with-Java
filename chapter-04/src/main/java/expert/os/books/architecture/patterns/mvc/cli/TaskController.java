6cd31ac7ca625c9a5040fef0c55e34123339e220
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-14 07:58:26 +0100

    feat: add TaskConsoleView to render task list and CLI errors

---
+++ chapter-04/src/main/java/expert/os/books/architecture/patterns/mvc/cli/TaskController.java
@@ -0,0 +1,25 @@
+package expert.os.books.architecture.patterns.mvc.cli;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+class TaskController {
+    private final TaskModel model;
+    private final TaskConsoleView view;
+
+    @Inject
+    public TaskController(TaskModel model, TaskConsoleView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void handleUserInput(String rawInput) {
+        try {
+            model.addTask(rawInput);
+            view.render(model);
+        } catch (IllegalArgumentException e) {
+            view.renderError(e.getMessage());
+        }
+    }
+}

f05d51029ca2d243d4e913296218780175f2854e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-25 18:27:11 +0100

    refactor: make StockQueryDto public

--- chapter-05/src/main/java/expert/os/books/architecture/patterns/remote/StockQueryDto.java
+++ chapter-05/src/main/java/expert/os/books/architecture/patterns/remote/StockQueryDto.java
@@ -1,4 +1,4 @@
 package expert.os.books.architecture.patterns.remote;
 
-record StockQueryDto(double minPrice, double maxPrice, String sector) {
+public record StockQueryDto(double minPrice, double maxPrice, String sector) {
 }

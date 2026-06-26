8e95fa1961f5ac968e64f091ddf4762404a33130
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-26 19:30:46 +0100

    chore: update StockQueryDto to accept multiple sectors in stock queries

--- chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/controller/StockQueryDto.java
+++ chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/controller/StockQueryDto.java
@@ -1,6 +1,7 @@
 package expert.os.books.architecture.patterns.remote.controller;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-public record StockQueryDto(BigDecimal minPrice, BigDecimal maxPrice, String sector) {
+public record StockQueryDto(BigDecimal minPrice, BigDecimal maxPrice, List<String> sectors) {
 }

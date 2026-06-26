1c5f5fe5d9874e2d7f105763cf92c0d1ab93cf98
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-26 19:22:48 +0100

    chore: add update, toString, and equality methods to Stock

--- chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/Stock.java
+++ chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/Stock.java
@@ -1,6 +1,7 @@
 package expert.os.books.architecture.patterns.remote;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Stock {
 
@@ -20,4 +21,31 @@ public class Stock {
     public BigDecimal getPrice() {
         return price;
     }
+
+    void update(BigDecimal newPrice) {
+        this.price = newPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "sticker='" + sticker + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Stock stock)) {
+            return false;
+        }
+        return Objects.equals(sticker, stock.sticker);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(sticker);
+    }
+
+
 }

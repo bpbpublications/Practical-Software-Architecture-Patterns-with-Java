8b03057f8e001a349e6e3f932da3c620f5e325d2
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-26 19:34:02 +0100

    chore: add logger statements to StockRepository methods

--- chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/StockRepository.java
+++ chapter-06/src/main/java/expert/os/books/architecture/patterns/remote/StockRepository.java
@@ -8,25 +8,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class StockRepository {
 
+    private static final Logger LOGGER = Logger.getLogger(StockRepository.class.getName());
+
     private Map<String, Stock> stocks = new HashMap<>();
 
     public void save(Stock stock) {
+        LOGGER.info("Saving stock: " + stock);
         stocks.put(stock.getSticker(), stock);
     }
 
     public Optional<Stock> findById(String sticker) {
+        LOGGER.info("Finding stock by sticker: " + sticker);
         return Optional.ofNullable(stocks.get(sticker));
     }
 
     public void delete(String sticker) {
+        LOGGER.info("Deleting stock with sticker: " + sticker);
         stocks.remove(sticker);
     }
 
     public List<Stock> find(List<String> sectors, BigDecimal minPrice, BigDecimal maxPrice) {
+        LOGGER.info("Finding stocks with sectors: " + sectors + ", minPrice: " + minPrice + ", maxPrice: " + maxPrice);
         Predicate<Stock> minPricePredicate = stock -> stock.getPrice().compareTo(minPrice) >= 0;
         Predicate<Stock> maxPricePredicate = stock -> stock.getPrice().compareTo(maxPrice) <= 0;
         Predicate<Stock> isSticker = stock -> sectors.isEmpty() || sectors.contains(stock.getSticker());

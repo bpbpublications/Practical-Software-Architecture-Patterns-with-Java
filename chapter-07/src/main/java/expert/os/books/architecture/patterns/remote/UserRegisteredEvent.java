5b3eb05725957b47c589535d029caa9829e7cc13
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 10:45:15 +0100

    refactor: use UUID for userId in user registered event

--- chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRegisteredEvent.java
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRegisteredEvent.java
@@ -3,9 +3,9 @@ package expert.os.books.architecture.patterns.remote;
 import java.time.Instant;
 import java.util.UUID;
 
-public record UserRegisteredEvent(UUID id, Instant timestamp, String userId, String name, String email) {
+public record UserRegisteredEvent(UUID id, Instant timestamp, UUID userId, String name, String email) {
 
-    public UserRegisteredEvent(String userId, String name, String email) {
+    public UserRegisteredEvent(UUID userId, String name, String email) {
         this(UUID.randomUUID(), Instant.now(), userId, name, email);
     }
 }

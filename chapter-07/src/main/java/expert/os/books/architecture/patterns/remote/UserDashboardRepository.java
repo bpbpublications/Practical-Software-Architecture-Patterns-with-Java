52f9d4196afe9264916bdec437ba08ffae8db1be
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 08:56:29 +0100

    refactor: add user dashboard repository

---
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserDashboardRepository.java
@@ -0,0 +1,27 @@
+package expert.os.books.architecture.patterns.remote;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+class UserDashboardRepository {
+
+    private static final Logger LOGGER = Logger.getLogger(UserDashboardRepository.class.getName());
+
+    private final Map<String, UserDashboard> data = new ConcurrentHashMap<>();
+
+    public Optional<UserDashboard> findDashboard(String userId) {
+        LOGGER.info("[READ MODEL] Performing sub-millisecond read from NoSQL database...");
+        return Optional.ofNullable(data.get(userId));
+    }
+
+    public void save(UserDashboard userDashboard) {
+        data.put(userDashboard.userId(), userDashboard);
+        LOGGER.info("[READ MODEL] Materialized view saved in NoSQL database for: " + userDashboard.userId());
+    }
+}

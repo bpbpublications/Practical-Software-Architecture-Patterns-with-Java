0f1bf09407d4842d5da76875180f87c236b54bdc
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 08:56:43 +0100

    refactor: add user registered event

---
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRegisteredEvent.java
@@ -0,0 +1,11 @@
+package expert.os.books.architecture.patterns.remote;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserRegisteredEvent(UUID id, Instant timestamp, String userId, String name, String email) {
+
+    public UserRegisteredEvent(String userId, String name, String email) {
+        this(UUID.randomUUID(), Instant.now(), userId, name, email);
+    }
+}

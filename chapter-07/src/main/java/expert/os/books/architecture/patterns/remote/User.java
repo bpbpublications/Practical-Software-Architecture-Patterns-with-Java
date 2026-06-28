a4092f523813e34db612bffb03ec3279accd0a12
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 10:50:54 +0100

    feat: add user model with UUID identity in remote package

---
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/User.java
@@ -0,0 +1,61 @@
+package expert.os.books.architecture.patterns.remote;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public User(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) {
+            return false;
+        }
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

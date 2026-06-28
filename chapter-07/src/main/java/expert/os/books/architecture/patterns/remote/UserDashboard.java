e5d96002b867c32e1e8372fa9bd51237eedcc885
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 11:24:39 +0100

    refactor: make UserDashboard record package-private

--- chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserDashboard.java
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserDashboard.java
@@ -2,4 +2,4 @@ package expert.os.books.architecture.patterns.remote;
 
 import java.util.UUID;
 
-public record UserDashboard(UUID userId, String name, String email) {}
+record UserDashboard(UUID userId, String name, String email) {}

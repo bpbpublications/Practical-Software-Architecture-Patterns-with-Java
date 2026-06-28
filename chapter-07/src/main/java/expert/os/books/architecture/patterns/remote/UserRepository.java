4ee4080d0b47a4190d964be6540ad66c97b6c697
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 12:59:06 +0100

    refactor: make UserRepository class package-private

--- chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRepository.java
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @ApplicationScoped
-public class UserRepository {
+class UserRepository {
 
     private Map<UUID, User> users = new java.util.concurrent.ConcurrentHashMap<>();
 

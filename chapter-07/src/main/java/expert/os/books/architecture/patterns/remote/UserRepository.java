e91dd3b453af6c196dc9c7e41b9b568a5630fb44
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-28 10:56:44 +0100

    feat: add user repository with in-memory concurrent storage

---
+++ chapter-07/src/main/java/expert/os/books/architecture/patterns/remote/UserRepository.java
@@ -0,0 +1,17 @@
+package expert.os.books.architecture.patterns.remote;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Map;
+import java.util.UUID;
+
+@ApplicationScoped
+public class UserRepository {
+
+    private Map<UUID, User> users = new java.util.concurrent.ConcurrentHashMap<>();
+
+    public void save(User user) {
+        users.put(user.getId(), user);
+    }
+
+}

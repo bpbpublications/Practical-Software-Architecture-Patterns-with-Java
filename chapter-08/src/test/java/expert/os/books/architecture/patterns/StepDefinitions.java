e0d7c1985f054547138cd3f10f6c02c323397bd6
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-11 06:48:59 +0100

    add StepDefinitions class with cucumber steps for example scenario

---
+++ chapter-08/src/test/java/expert/os/books/architecture/patterns/StepDefinitions.java
@@ -0,0 +1,25 @@
+package expert.os.books.architecture.patterns;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class StepDefinitions {
+
+
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
